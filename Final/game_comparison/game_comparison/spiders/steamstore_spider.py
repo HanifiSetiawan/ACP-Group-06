7dca4edfc88d93e817d3178beaaa9431057969f7
Hanifi Abrar Setiawan <hanifisetiawan@gmail.com>
2025-06-02 10:54:36 +0800

    Steam scraper update

---
+++ Final/game_comparison/game_comparison/spiders/steamstore_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class SteamStoreSpider(scrapy.Spider):
+    name = "steamstore"
+    allowed_domains = ["store.steampowered.com"]
+    start_urls = [
+        "https://store.steampowered.com/search/?specials=1"
+    ]
+
+    def parse(self, response):
+        for row in response.css('a.search_result_row'):
+            name = row.css('span.title::text').get()
+            discount = row.css('div.discount_pct::text').get()
+            original_price = row.css('div.discount_original_price::text').get()
+            final_price = row.css('div.discount_final_price::text').get()
+
+            if name and final_price:
+                yield {
+                    "name": name.strip(),
+                    "discount": discount.strip() if discount else None,
+                    "original_price": original_price.strip() if original_price else None,
+                    "final_price": final_price.strip(),
+                }
